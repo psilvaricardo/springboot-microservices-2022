b0a22418319aaee53b14e6bc9b325ae9f7b9c8d0
Ricardo Plascencia Silva <ricardo072@gmail.com>
2022-11-27 21:34:41 -0600

    Adding SpringBoot WebFlux dependency

--- movie-catalog-mservice/src/main/java/com/microservicesws/controller/MovieCatalogController.java
+++ movie-catalog-mservice/src/main/java/com/microservicesws/controller/MovieCatalogController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Collections;
 import java.util.List;
@@ -18,7 +19,10 @@ public class MovieCatalogController {
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         log.info("MovieCatalogController calling getCatalog {}", userId);
 
-        // dummy API, hardcoded temporally for the proof of concept
+        // get all the rated movie IDs
+        // for each movie Id, call the movie info service and get details
+        // lastly, put them all together
+
         return Collections.singletonList(
                 new CatalogItem("Transformers", "Test", 4)
         );
