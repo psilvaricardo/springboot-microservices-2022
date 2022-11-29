17ca96e51a45669e7774c66df3fba223a4b0b6c7
Ricardo Plascencia Silva <ricardo072@gmail.com>
2022-11-29 13:48:06 -0600

    Register Eureka Clients with the Eureka server

--- movie-catalog-mservice/src/main/java/com/microservicesws/MovieCatalogMserviceApplication.java
+++ movie-catalog-mservice/src/main/java/com/microservicesws/MovieCatalogMserviceApplication.java
@@ -2,8 +2,10 @@ package com.microservicesws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogMserviceApplication {
 
 	public static void main(String[] args) {
