ddd5b1bc39cb2acbf19d70c9871d09cfef18cf67
Ricardo Plascencia Silva <ricardo072@gmail.com>
2022-11-30 18:15:54 -0600

    Fixing WebClient logic as part of API call

--- movie-info-mservice/src/main/java/com/microservicesws/controller/MovieInfoController.java
+++ movie-info-mservice/src/main/java/com/microservicesws/controller/MovieInfoController.java
@@ -1,22 +1,41 @@
 package com.microservicesws.controller;
 
-import com.microservicesws.model.Movie;
+import com.microservicesws.model.MovieInfo;
+import com.microservicesws.model.themoviedb.Movie;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Slf4j
 @RestController
 @RequestMapping("/movies")
 public class MovieInfoController {
 
-    @RequestMapping("/{movieId}")
-    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-        log.info("MovieInfoController calling getMovieInfo {}", movieId);
+    @Autowired
+    private WebClient webClient;
 
-        // dummy API, hardcoded temporally for the proof of concept
-        return new Movie(movieId, "Test");
+    @RequestMapping("/{movieId}")
+    public MovieInfo getMovieInfo(@PathVariable("movieId") String movieId) {
+        log.info("MovieInfoController calling getMovieInfo from external MovieDB service {}", movieId);
+
+        String urlStr = "/3/movie/"+movieId;
+        log.info("MovieInfoController calling MovieDB service: {}", urlStr);
+
+        // making a REST API call to the external movie db service
+        Movie movie = webClient
+                       .get()
+                       .uri("/3/movie/{movieId}", movieId)
+                       .accept(MediaType.APPLICATION_JSON)
+                       .retrieve()
+                       .bodyToMono(Movie.class)
+                       .block();
+
+        log.info("MovieInfoController returning: {}", movie);
+        return new MovieInfo(movieId, movie.getOriginalTitle(), movie.getOverview());
     }
 
 }
---
+++ movie-info-mservice/src/main/java/com/microservicesws/service/WebClientBean.java
@@ -0,0 +1,37 @@
+package com.microservicesws.service;
+
+import com.microservicesws.util.MovieInfoConstants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Slf4j
+@Service
+public class WebClientBean {
+
+    private final WebClient webClient;
+
+    @Autowired
+    public WebClientBean(WebClient.Builder builder) {
+        log.info("WebClientBean Ctor...");
+        String access_token = MovieInfoConstants.getValueOf("access_token");
+
+        this.webClient =
+                builder
+                .baseUrl("https://api.themoviedb.org")
+                .defaultHeaders( httpHeaders -> {
+                            httpHeaders.add("Authorization","Bearer "+access_token);
+                        }
+                )
+                .build();
+
+    }
+
+    @Bean
+    @LoadBalanced
+    public WebClient loadBalancedWebClientBuilder() { return this.webClient; }
+
+}
