9574b6547424d6ee5a992cb6de11d5ca0224e0e0
Ricardo Plascencia Silva <ricardo072@gmail.com>
2022-12-06 14:24:50 -0600

    Breaking up API calls as individual methods

--- movie-catalog-mservice/src/main/java/com/microservicesws/controller/MovieCatalogController.java
+++ movie-catalog-mservice/src/main/java/com/microservicesws/controller/MovieCatalogController.java
@@ -6,6 +6,7 @@ import com.microservicesws.model.Rating;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,14 +32,8 @@ public class MovieCatalogController {
 
         // https://www.baeldung.com/spring-webclient-json-list
         // making a REST API call to the data ms
-        Mono<List<Rating>> response =
-                webClientBuilder.build()
-                        .get()
-                        .uri("http://movie-data-mservice/ratingsdata/users/"+ userId)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .retrieve()
-                        .bodyToMono(new ParameterizedTypeReference<List<Rating>>() {});
-                        //.retryWhen(Retry.max(3));
+        Mono<List<Rating>> response = getResponseFromDataService(userId);
+
         // get all the rated movie IDs
         List<Rating> ratings = response.block();
 
@@ -48,18 +43,42 @@ public class MovieCatalogController {
                 .stream()
                 .map( rating -> {
                     // making the API call
-                    MovieInfo movieInfo =
-                            webClientBuilder.build()
-                            .get()
-                            .uri("http://movie-info-mservice/movies/"+ rating.getMovieId())
-                            .retrieve()
-                            .bodyToMono(MovieInfo.class)
-                            //.retryWhen(Retry.max(3))
-                            .block();
+                    MovieInfo movieInfo = getResponseFromMovieInfo(rating);
                     return new CatalogItem(movieInfo.getName(), movieInfo.getDescription(), rating.getRating());
                     }
                 )
                 .collect(Collectors.toList()); // lastly, put them all together
+    }
+
+    private Mono<List<Rating>> getResponseFromDataService(String userId) {
+        return webClientBuilder.build()
+                .get()
+                .uri("http://movie-data-mservice/ratingsdata/users/" + userId)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                /*
+                    .onStatus(HttpStatus::isError, resp -> {
+                        if (resp.statusCode().is4xxClientError()) {
+                            log.error("Error: 4xx response from movie-data-mservice");
+                            return Mono.error(new Exception("Error: 4xx response from movie-data-mservice"));
+                        } else if (resp.statusCode().is5xxServerError()) {
+                            log.error("Error: 5xx response from movie-data-mservice");
+                            return Mono.error(new Exception("Error: 5xx response from movie-data-mservice"));
+                        }
+                    log.error("Error: HTTP " + resp.statusCode() + " response from movie-data-mservice");
+                    return Mono.error(new Exception("Error: HTTP " + resp.statusCode() + " response from movie-data-mservice"));
+                    })*/
+                .bodyToMono(new ParameterizedTypeReference<List<Rating>>() {
+                });
+    }
+
+    private MovieInfo getResponseFromMovieInfo(Rating rating) {
+        return webClientBuilder.build()
+                .get()
+                .uri("http://movie-info-mservice/movies/" + rating.getMovieId())
+                .retrieve()
+                .bodyToMono(MovieInfo.class)
+                .block();
     }
 
 }
